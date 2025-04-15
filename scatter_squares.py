7c59885fee2ed4f716aecbb4cf90bc8b48375122
Ethan Brooks <1156270547@qq.com>
2025-04-15 15:26:02 +0800

    Add random_walk.py and rw_visual.py for random walk simulation and visualization

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,15 +1,16 @@
 import matplotlib.pyplot as plt
+
 x_value = range(1, 1001)
-y_value = [value**2 for value in x_value]
+y_value = [value ** 2 for value in x_value]
 
 plt.style.use('seaborn-v0_8')  # 更改网格的样式
 fig, ax = plt.subplots()
 
-ax.scatter(x_value, y_value, c=y_value, cmap=plt.cm.Blues, s = 10) # s参数设置
+ax.scatter(x_value, y_value, c=y_value, cmap=plt.cm.Blues, s=10)  # s参数设置
 ax.set_title('Square Number', fontsize=24)
 ax.set_xlabel('Value', fontsize=14)
 ax.set_ylabel('Squares of Value', fontsize=14)
-ax.tick_params(labelsize=14) # 设置刻度标记的样式
-ax.axis([0, 1100, 0, 1_100_000]) # 设置坐标轴的范围,参数是一个列表
+ax.tick_params(labelsize=14)  # 设置刻度标记的样式
+ax.axis([0, 1100, 0, 1_100_000])  # 设置坐标轴的范围,参数是一个列表
 
 plt.show()
