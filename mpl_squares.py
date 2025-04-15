23343b5462b4c593ac3f0ad6731612fcb64b14c4
Ethan Brooks <1156270547@qq.com>
2025-04-15 15:40:21 +0800

    Add random_walk.py and rw_visual.py for random walk simulation and visualization

--- mpl_squares.py
+++ mpl_squares.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
-plt.style.use('Solarize_Light2')  #更改网格的样式
+plt.style.use('Solarize_Light2')  # 更改网格的样式
 
 fig, ax = plt.subplots()  # subplots()函数可在一个图形中绘制一个或多个绘图。变量fig表示由多个绘图绘成的整个图形，变量ax表示图形中的绘图
 ax.plot(input_values, squares, linewidth=3)  # linewidth参数设置线条的宽度
